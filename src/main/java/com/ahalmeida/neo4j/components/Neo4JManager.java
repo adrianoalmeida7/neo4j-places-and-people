80de57ed46c8a821b810ec834207c9ea27d74610
Adriano Almeida <adrianoalmeida7@gmail.com>
2011-06-09 02:09:46 -0300

    externalizing configuration directory to web.xml

--- src/main/java/com/ahalmeida/neo4j/components/Neo4JManager.java
+++ src/main/java/com/ahalmeida/neo4j/components/Neo4JManager.java
@@ -2,6 +2,7 @@ package com.ahalmeida.neo4j.components;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
 
 import org.apache.log4j.Logger;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
@@ -13,14 +14,19 @@ import br.com.caelum.vraptor.ioc.ComponentFactory;
 @Component
 @ApplicationScoped
 public class Neo4JManager implements ComponentFactory<EmbeddedGraphDatabase> {
-	private static final String DATABASE_DIR = "/Users/ahalmeida/Projects/java/neo4j-vraptor/graphdb";
+	
+	private final String databaseDirectory;
 	private Logger LOG = Logger.getLogger(Neo4JManager.class);
 	private EmbeddedGraphDatabase db;
 
+	public Neo4JManager(ServletContext ctx) {
+		this.databaseDirectory = ctx.getInitParameter("neo4j_database_dir");
+	}
+	
 	@PostConstruct
 	public void initialize() {
-		LOG.info("Starting up the graph database at " + DATABASE_DIR);
-		db = new EmbeddedGraphDatabase(DATABASE_DIR);
+		LOG.info("Starting up the graph database at " + databaseDirectory);
+		db = new EmbeddedGraphDatabase(databaseDirectory);
 	}
 
 	public EmbeddedGraphDatabase getInstance() {
@@ -29,7 +35,7 @@ public class Neo4JManager implements ComponentFactory<EmbeddedGraphDatabase> {
 
 	@PreDestroy
 	public void destroy() {
-		LOG.info("Shutting down the database at " + DATABASE_DIR);
+		LOG.info("Shutting down the database at " + databaseDirectory);
 		db.shutdown();
 	}
 }
